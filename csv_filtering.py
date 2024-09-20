6b0694b1534c1b15980e57bf7e2ed4aacb1606e5
tavo <tavo.lop33@gmail.com>
2024-09-19 22:09:56 -0300

    termino el punto 1 y 2

--- csv_filtering.py
+++ csv_filtering.py
@@ -1,40 +1,76 @@
 import pandas as pd
 import numpy as np
+from scipy import stats as st
 
 
-def calculate_r(x, y):
-    sxi = np.sum(x)
-    syi = np.sum(y)
-    n = len(x)
+def calculate_stats(x, y):
+    x = np.array(x)
+    y = np.array(y)
 
-    sumprod = 0.0
-    sumxpow2 = 0.0
-    for i in range(n):
-        sumprod += x[i] * y[i]
-        sumxpow2 += np.pow(x[i], 2)
+    n = x.size
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    sumxy = np.sum(x * y)
+    sumx = np.sum(x)
+    sumx2 = np.sum(x**2)
+    sumy = np.sum(y)
 
-    sxy = sumprod - ((sxi * syi) / n)
-    sxx = sumxpow2 - (np.pow(sxi, 2) / n)
-    # b1 = sxy / sxx
+    Sxy = sumxy - (sumx * sumy) / n
+    Sxx = sumx2 - sumx**2 / n
+    Syy = np.sum((y - y_mean)**2)
 
-    # xmean = np.mean(x)
-    ymean = np.mean(y)
-    # b0 = ymean - b1 * xmean
+    b1 = Sxy / Sxx
+    b0 = y_mean - b1 * x_mean
 
-    syy = 0.0
-    for i in range(n):
-        syy += np.pow(y[i] - ymean, 2)
+    regy = b0 + b1*x
 
-    # stc = syy
-    # sce = syy - (np.pow(sxy, 2) / sxx)
-    # stdpow2 = sce / (n - 2)
-    # r2 = 1 - (sce / stc)
-    r = sxy / np.sqrt(sxx * syy)
+    SCE = np.sum((y - regy)**2)
+    SCR = np.sum((regy - y_mean)**2)
+    STC = Syy
 
-    return r
+    R2 = 1 - (SCE / STC) # o también SCR / STC
+
+    r = Sxy / np.sqrt(Sxx * Syy)
+    sigma2 = SCE / (n - 2)
+
+    T = b1 / np.sqrt(sigma2 / Sxx)
+
+    return {
+            "n": n,
+            "x_mean": x_mean,
+            "y_mean": y_mean,
+            "Sxy": Sxy,
+            "Sxx": Sxx,
+            "Syy": Syy,
+            "b0": b0,
+            "b1": b1,
+            "SCE": SCE,
+            "SCR": SCR,
+            "R2": R2,
+            "r": r,
+            "sigma2": sigma2,
+            "T": T
+        }
 
 
-file_path = "players_21.csv"
+def significance_test(data):
+    b1 = data['b1'] 
+    sigma2 = data['sigma2']
+    Sxx = data['Sxx']
+    n = data['n']
+    alfa = 0.05
+
+    T_b1 = np.abs(b1 / np.sqrt(sigma2 / Sxx))
+    t_b1 = st.t.ppf(1-alfa/2, n-2)
+    if(T_b1 > t_b1):
+        results = "x tiene cierta influencia sobre y"
+    else:
+        results = "x no tiene influencia sobre y"
+
+    return results
+
+
+file_path = "../players_21.csv"
 df = pd.read_csv(file_path, na_values=["", " ", "NA", "N/A", "nan", "NaN"])
 
 columns_to_keep = [
@@ -46,34 +82,31 @@ columns_to_keep = [
     "value_eur",
     "wage_eur",
     "skill_moves",
-    # "pace",
-    # "shooting",
-    # "passing",
-    # "dribbling",
-    # "defending",
-    # "physic",
 ]
 df_filtered = df[columns_to_keep]
 
-df_cleaned_rows = df_filtered.dropna()
-
-# print(f"Filas antes de limpiar: {len(df_filtered)}")
-# print(f"Filas después de limpiar: {len(df_cleaned_rows)}")
-
 r_values = {}
 y_column = "value_eur"
-y_values = df_cleaned_rows[y_column].values
+y_values = df_filtered[y_column].values
 for x_column in columns_to_keep:
     if x_column != y_column:
-        x_values = df_cleaned_rows[x_column].values
-        r = calculate_r(x_values, y_values)
-        r_values[x_column] = r
+        x_values = df_filtered[x_column].values
+        stats = calculate_stats(x_values, y_values)
+        r_values[x_column] = stats["r"]
 
 best_x_column = max(r_values, key=r_values.get)
 best_r_value = r_values[best_x_column]
 
+for key, value in sorted(r_values.items(), key=lambda item: item[1], reverse=True):
+    print(f"Columna: {key} -> r: {value}")
+
 print(f"La columna '{best_x_column}' produce el mayor valor de r: {best_r_value}")
-for key, value in r_values.items():
-    print(f"{key}: {value}")
+# por lo tanto hacemos el analisis con esta columna
+
+print(significance_test(stats))
+
+stats = calculate_stats(df_filtered[best_x_column].values, y_values)
+print("Coeficientes de correlación lineal r y de determinación r² son: ", stats["r"], stats["R2"])
+
 
 # df_cleaned_rows.to_csv("players_21_filtered2.csv", index=False)
